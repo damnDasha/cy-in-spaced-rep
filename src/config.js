a50fc1bb2710f76920ef8ad1aff57d25d29c38a4
dasha akhten <dasha.akhten@gmail.com>
2022-07-13 20:41:31 -0700

    feat: update cy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-  API_ENDPOINT: "https://gentle-reef-38657.herokuapp.com",
+  API_ENDPOINT: "http://localhost:8000",
+  // API_ENDPOINT: "https://gentle-reef-38657.herokuapp.com",
   TOKEN_KEY: "blogful-client-auth-token",
 };
